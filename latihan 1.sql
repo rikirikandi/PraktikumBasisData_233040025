d1469d3296c32e16dce06377186890351f50e94d
rikirikandi <rikirikandi1242@gmail.com>
2024-10-09 13:26:16 +0700

    memperbaiki latihan 1

---
+++ latihan 1.sql
@@ -0,0 +1,8 @@
+INSERT INTO Mahasiswa (ID, NIM, Nama, Jurusan, TanggalLahir)
+VAlUES 
+(1, 233040020, 'Budi Santoso' , 'Teknik Informatika', '2002-02-02'),
+(2, 233040027, 'Riki Rikandi' , 'Teknik Informatika', '2005-05-29'),
+(3, 233040025, 'Riyyadu' , 'Teknik Informatika', '2002-02-22'),
+(4, 203040021, 'Biagi' , 'Teknik Informatika', '2005-02-15'),
+(5, 203040022, 'Marchiko' , 'Teknik Informatika', '2001-02-15'),
+(6, 203040023, 'Humaira' , 'Teknik Informatika', '2005-02-15');
